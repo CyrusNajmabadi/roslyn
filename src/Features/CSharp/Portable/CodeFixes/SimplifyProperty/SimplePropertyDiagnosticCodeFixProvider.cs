a731ca539426761fcf11eb0d8a49259a7b563b45
CyrusNajmabadi <cyrusn@microsoft.com>
2016-02-21 17:40:35 -0800

    Add feature to simplify properties.

---
+++ src/Features/CSharp/Portable/CodeFixes/SimplifyProperty/SimplePropertyDiagnosticCodeFixProvider.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Roslyn.Utilities;
+
+namespace Microsoft.CodeAnalysis.CSharp.CodeFixes.SimplifyProperty
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp), Shared]
+    internal class SimplePropertyDiagnosticCodeFixProvider : CodeFixProvider
+    {
+        public override ImmutableArray<string> FixableDiagnosticIds { get; } =
+            ImmutableArray.Create(IDEDiagnosticIds.SimplifyPropertyId);
+
+        public override FixAllProvider GetFixAllProvider() => BatchFixAllProvider.Instance;
+
+        public override Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            context.RegisterCodeFix(new MyCodeAction("SimplifyProperty", 
+                c => UpdateSolutionAsync(context, c)), context.Diagnostics);
+            return SpecializedTasks.EmptyTask;
+        }
+
+        private async Task<Solution> UpdateSolutionAsync(CodeFixContext context, CancellationToken cancellationToken)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+            var propertyDeclaration = (PropertyDeclarationSyntax)root.FindNode(context.Span);
+
+            var returnStatement = (ReturnStatementSyntax)propertyDeclaration.AccessorList.Accessors[0].Body.Statements[0];
+            var newPropertyDeclaration = propertyDeclaration
+                .WithAccessorList(null)
+                .WithExpressionBody(SyntaxFactory.ArrowExpressionClause(returnStatement.Expression))
+                .WithSemicolonToken(returnStatement.SemicolonToken)
+                .WithTrailingTrivia(propertyDeclaration.AccessorList.GetTrailingTrivia());
+
+            var newDocument = context.Document.WithSyntaxRoot(root.ReplaceNode(
+                propertyDeclaration, newPropertyDeclaration));
+
+            return newDocument.Project.Solution;
+        }
+
+        private class MyCodeAction : CodeAction.SolutionChangeAction
+        {
+            public MyCodeAction(
+                string title,
+                Func<CancellationToken, Task<Solution>> createChangedSolution) 
+                : base(title, createChangedSolution, title)
+            {
+            }
+        }
+    }
+}
---
+++ src/Features/CSharp/Portable/CodeFixes/SimplifyProperty/SimplifyPropertyDiagnosticAnalyzer.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Microsoft.CodeAnalysis.CSharp.CodeFixes.SimplifyProperty
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    internal class SimplifyPropertyDiagnosticAnalyzer : DiagnosticAnalyzer
+    {
+        private static readonly DiagnosticDescriptor s_descriptor = new DiagnosticDescriptor(
+            IDEDiagnosticIds.SimplifyPropertyId,
+            "SimplifyProperty",
+            "SimplifyProperty",
+            DiagnosticCategory.Style,
+            DiagnosticSeverity.Hidden,
+            isEnabledByDefault: true,
+            customTags: DiagnosticCustomTags.Unnecessary);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } =
+            ImmutableArray.Create(s_descriptor);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(ProcessNode, SyntaxKind.PropertyDeclaration);
+        }
+
+        private void ProcessNode(SyntaxNodeAnalysisContext context)
+        {
+            var propertyDeclaration = (PropertyDeclarationSyntax)context.Node;
+            if (propertyDeclaration.AccessorList.Accessors.Count != 1 ||
+                !propertyDeclaration.AccessorList.Accessors[0].IsKind(SyntaxKind.GetAccessorDeclaration))
+            {
+                return;
+            }
+
+            var accessor = propertyDeclaration.AccessorList.Accessors[0];
+            if (!accessor.Body.IsKind(SyntaxKind.Block))
+            {
+                return;
+            }
+
+            if (accessor.AttributeLists.Count > 0)
+            {
+                return;
+            }
+
+            var block = (BlockSyntax)accessor.Body;
+            if (block.Statements.Count != 1)
+            {
+                return;
+            }
+
+            var statement = block.Statements[0];
+            if (!statement.IsKind(SyntaxKind.ReturnStatement))
+            {
+                return;
+            }
+
+            var returnStatement = (ReturnStatementSyntax)statement;
+            if (returnStatement.Expression == null)
+            {
+                return;
+            }
+
+            if (!IsSimpleExpression(returnStatement.Expression))
+            {
+                return;
+            }
+
+            if (HasAnyNonWhitespaceTrivia(accessor))
+            {
+                return;
+            }
+
+            context.ReportDiagnostic(Diagnostic.Create(
+                s_descriptor, propertyDeclaration.GetLocation()));
+        }
+
+        private bool IsSimpleExpression(ExpressionSyntax expression)
+        {
+            if (expression.IsKind(SyntaxKind.IdentifierName))
+            {
+                return true;
+            }
+            else if (expression.IsKind(SyntaxKind.SimpleMemberAccessExpression))
+            {
+                var accessExpression = (MemberAccessExpressionSyntax)expression;
+                return IsSimpleExpression(accessExpression.Expression);
+            }
+            else if (expression.IsKind(SyntaxKind.ThisExpression) || 
+                expression.IsKind(SyntaxKind.BaseExpression))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool HasAnyNonWhitespaceTrivia(AccessorDeclarationSyntax accessor)
+        {
+            return accessor.DescendantTrivia().Any(t => 
+                !t.IsKind(SyntaxKind.WhitespaceTrivia) &&
+                !t.IsKind(SyntaxKind.EndOfLineTrivia));
+        }
+    }
+}
