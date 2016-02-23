fb26ab08a3c21dc89a2263a27c691983b720b2ab
CyrusNajmabadi <cyrusn@microsoft.com>
2016-02-22 23:50:41 -0800

    Add the feature code.

---
+++ src/Features/CSharp/Portable/InvokeDelegateWithConditionalAccess/RemovePrivateSetterAnalyzer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Microsoft.CodeAnalysis.CSharp.InvokeDelegateWithConditionalAccess
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    internal class RemovePrivateSetterAnalyzer : DiagnosticAnalyzer
+    {
+        public static readonly DiagnosticDescriptor Descriptor = new DiagnosticDescriptor(
+            nameof(RemovePrivateSetterAnalyzer),
+            nameof(RemovePrivateSetterAnalyzer),
+            nameof(RemovePrivateSetterAnalyzer),
+            DiagnosticCategory.Style,
+            DiagnosticSeverity.Hidden,
+            isEnabledByDefault: true,
+            customTags: DiagnosticCustomTags.Unnecessary);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(Descriptor);
+
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(ProcessNode, SyntaxKind.SetAccessorDeclaration);
+        }
+
+        private void ProcessNode(SyntaxNodeAnalysisContext context)
+        {
+            var accessor = (AccessorDeclarationSyntax)context.Node;
+            if (accessor.Modifiers.Any(SyntaxKind.PrivateKeyword) &&
+                accessor.Body == null)
+            {
+                context.ReportDiagnostic(Diagnostic.Create(
+                    Descriptor, accessor.GetLocation()));
+            }
+        }
+    }
+}
---
+++ src/Features/CSharp/Portable/InvokeDelegateWithConditionalAccess/RemovePrivateSetterCodeFixProvider.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Roslyn.Utilities;
+
+namespace Microsoft.CodeAnalysis.CSharp.InvokeDelegateWithConditionalAccess
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp), Shared]
+    internal class RemovePrivateSetterCodeFixProvider : CodeFixProvider
+    {
+        public override ImmutableArray<string> FixableDiagnosticIds { get; } =
+            ImmutableArray.Create(RemovePrivateSetterAnalyzer.Descriptor.Id);
+
+        public override FixAllProvider GetFixAllProvider() => BatchFixAllProvider.Instance;
+
+        public override Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            context.RegisterCodeFix(new MyCodeAction(
+                nameof(RemovePrivateSetterAnalyzer),
+                c => UpdateDocumentAsync(context, c)), context.Diagnostics);
+            return SpecializedTasks.EmptyTask;
+        }
+
+        private async Task<Solution> UpdateDocumentAsync(CodeFixContext context, CancellationToken cancellationToken)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+            var node = (AccessorDeclarationSyntax)root.FindNode(context.Span);
+            var newRoot = root.RemoveNode(node, SyntaxRemoveOptions.AddElasticMarker);
+            return context.Document.WithSyntaxRoot(newRoot).Project.Solution;
+        }
+
+        private class MyCodeAction : CodeAction.SolutionChangeAction
+        {
+            public MyCodeAction(string title, Func<CancellationToken, Task<Solution>> createChangedSolution) 
+                : base(title, createChangedSolution, title)
+            {
+            }
+        }
+    }
+}
