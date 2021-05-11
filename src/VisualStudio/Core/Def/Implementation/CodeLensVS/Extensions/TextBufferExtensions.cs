24bc55f3e64d858e6f5200b898cd785f3e4874a7
Cyrus Najmabadi <cyrusn@microsoft.com>
2021-05-11 12:35:44 -0700

    Add extensions

---
+++ src/VisualStudio/Core/Def/Implementation/CodeLensVS/Extensions/TextBufferExtensions.cs
@@ -0,0 +1,32 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.VisualStudio.Text;
+
+namespace Microsoft.VisualStudio.LanguageServices.Implementation.CodeLensVS.Extensions
+{
+    internal static class TextBufferExtensions
+    {
+        /// <summary>
+        /// Gets the moniker for the text buffer if it exists, null otherwise
+        /// </summary>
+        /// <param name="textBuffer">Text buffer</param>
+        /// <returns>Document moniker</returns>
+        public static string GetDocumentMoniker(this ITextBuffer textBuffer)
+        {
+            ITextDocument document;
+            if ((textBuffer == null) || (!textBuffer.Properties.TryGetProperty(typeof(ITextDocument), out document)))
+            {
+                return null;
+            }
+
+            if (document == null)
+            {
+                return null;
+            }
+
+            return document.FilePath;
+        }
+    }
+}
