e3fc4ddf6923a5670defa73aa36f9b429ca3207f
Cyrus Najmabadi <cyrusn@microsoft.com>
2021-05-11 12:37:22 -0700

    Add parsing itnerfaces

---
+++ src/VisualStudio/Core/Def/Implementation/CodeLensVS/Parser/INodeInfo.cs
@@ -0,0 +1,28 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.VisualStudio.Text;
+
+namespace Microsoft.VisualStudio.LanguageServices.Implementation.CodeLensVS.Parser
+{
+    /// <summary>
+    /// Defines an object that has information on a node that was found from a syntax node visitor.
+    /// </summary>
+    internal interface INodeInfo
+    {
+        /// <summary>
+        /// Returns the starting position of the node.
+        /// </summary>
+        int Start
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Returns a syntax node info object for this found node.
+        /// </summary>
+        /// <param name="textSnapshot">A text snapshot that the locator uses to translate paths between snapshots</param>
+        SyntaxNodeInfo CreateSyntaxNodeInfo(ITextSnapshot textSnapshot);
+    }
+}
---
+++ src/VisualStudio/Core/Def/Implementation/CodeLensVS/Parser/IParsingService.cs
@@ -0,0 +1,35 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Microsoft.VisualStudio.LanguageServices.Implementation.CodeLensVS.Parser
+{
+    internal interface IParsingService
+    {
+        /// <summary>
+        /// Parse the provided text into a syntax tree.
+        /// </summary>
+        /// <param name="text">Text to parse.</param>
+        /// <returns>A syntax tree from parsing text.</returns>
+        SyntaxTree Parse(SourceText text);
+
+        /// <summary>
+        /// Called to visit a syntax tree.
+        /// </summary>
+        /// <param name="tree">The syntax tree</param>
+        /// <param name="onNodeFound">A callback action</param>
+        /// <param name="cancellationToken">A cancellation token that can be used to cancel the syntax tree visit</param>
+        void VisitSyntaxTree(SyntaxTree tree, Action<INodeInfo> onNodeFound, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Called to determine if current ParsingService understands the current SyntaxTree. If it doesn't, we reparse.
+        /// </summary>
+        /// <param name="tree">The syntax tree</param>
+        bool IsValidSyntaxTree(SyntaxTree tree);
+    }
+}
