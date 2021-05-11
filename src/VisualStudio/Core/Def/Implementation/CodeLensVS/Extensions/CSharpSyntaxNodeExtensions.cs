44f2ba8cf8932fd1549f89dfb2556a7be8ad5849
Cyrus Najmabadi <cyrusn@microsoft.com>
2021-05-11 12:43:17 -0700

    Add extensions

---
+++ src/VisualStudio/Core/Def/Implementation/CodeLensVS/Extensions/CSharpSyntaxNodeExtensions.cs
@@ -0,0 +1,90 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Diagnostics;
+using System.Globalization;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Microsoft.VisualStudio.LanguageServices.Implementation.CodeLensVS.Extensions
+{
+    internal static class CSharpSyntaxNodeExtensions
+    {
+        /// <summary>
+        /// Get the name of the identifier for the node.
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1800:DoNotCastUnnecessarily", Justification = "Too much memory to create all types of objects")]
+        public static string GetIdentifierName(SyntaxNode node)
+        {
+            if (node is NamespaceDeclarationSyntax)
+            {
+                return RemoveTrivia(((NamespaceDeclarationSyntax)node).Name).ToString();
+            }
+            else if (node is ClassDeclarationSyntax)
+            {
+                return ((ClassDeclarationSyntax)node).Identifier.ToString();
+            }
+            else if (node is StructDeclarationSyntax)
+            {
+                return ((StructDeclarationSyntax)node).Identifier.ToString();
+            }
+            else if (node is InterfaceDeclarationSyntax)
+            {
+                return ((InterfaceDeclarationSyntax)node).Identifier.ToString();
+            }
+            else if (node is EnumDeclarationSyntax)
+            {
+                return ((EnumDeclarationSyntax)node).Identifier.ToString();
+            }
+            else if (node is PropertyDeclarationSyntax)
+            {
+                PropertyDeclarationSyntax propNode = node as PropertyDeclarationSyntax;
+                string id = propNode.Identifier.ToString();
+                return propNode.ExplicitInterfaceSpecifier != null
+                    ? string.Format(CultureInfo.InvariantCulture, "({0}.{1})", RemoveTrivia(propNode.ExplicitInterfaceSpecifier.Name).ToString(), id)
+                    : id;
+            }
+            else if (node is MethodDeclarationSyntax)
+            {
+                return ((MethodDeclarationSyntax)node).Identifier.ToString();
+            }
+            else if (node is OperatorDeclarationSyntax)
+            {
+                OperatorDeclarationSyntax operatorNode = (OperatorDeclarationSyntax)node;
+
+                return string.Join(" ", operatorNode.OperatorKeyword.ToString(), operatorNode.OperatorToken.ToString());
+            }
+            else if (node is IndexerDeclarationSyntax)
+            {
+                IndexerDeclarationSyntax indexerNode = node as IndexerDeclarationSyntax;
+                string id = indexerNode.ThisKeyword.ToString();
+                return indexerNode.ExplicitInterfaceSpecifier != null
+                    ? string.Format(CultureInfo.InvariantCulture, "({0}.{1})", RemoveTrivia(indexerNode.ExplicitInterfaceSpecifier.Name).ToString(), id)
+                    : id;
+            }
+            else if (node is ConstructorDeclarationSyntax)
+            {
+                return ((ConstructorDeclarationSyntax)node).Identifier.ToString();
+            }
+            else if (node is DestructorDeclarationSyntax)
+            {
+                return string.Format(CultureInfo.InvariantCulture, "~{0}", ((DestructorDeclarationSyntax)node).Identifier.ToString());
+            }
+            else if (node is CompilationUnitSyntax)
+            {
+                return string.Empty;
+            }
+            else
+            {
+                Debug.Assert(false, "Unhandled node type");
+                return string.Empty;
+            }
+        }
+
+        private static TSyntaxNode RemoveTrivia<TSyntaxNode>(TSyntaxNode node) where TSyntaxNode : SyntaxNode
+        {
+            return node.ReplaceTrivia(node.DescendantTrivia(), (originalTrivia, replacementTrivia) => default(SyntaxTrivia));
+        }
+    }
+}
---
+++ src/VisualStudio/Core/Def/Implementation/CodeLensVS/Parser/CSharpSyntaxNodeVisitor.cs
@@ -0,0 +1,238 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.VisualStudio.Text;
+
+namespace Microsoft.VisualStudio.LanguageServices.Implementation.CodeLensVS.Parser
+{
+    internal class CSharpSyntaxNodeVisitor : CSharpSyntaxVisitor
+    {
+        /// <summary>
+        /// Callback action.
+        /// </summary>
+        private readonly Action<INodeInfo> onNodeFound;
+
+        /// <summary>
+        /// Token that can be used to cancel the visitor as we're walking the tree.
+        /// </summary>
+        private readonly CancellationToken cancellationToken;
+
+        /// <summary>
+        /// Creates the visitor.
+        /// </summary>
+        /// <param name="onNodeFound">Callback for when we find a node</param>
+        /// <param name="cancellationToken">A cancellation token that can stop the walk before it finishes</param>
+        public CSharpSyntaxNodeVisitor(Action<INodeInfo> onNodeFound, CancellationToken cancellationToken)
+        {
+            ArgumentValidation.NotNull(onNodeFound, "onNodeFound");
+
+            this.onNodeFound = onNodeFound;
+            this.cancellationToken = cancellationToken;
+        }
+
+        /// <summary>
+        /// Visits a compilation node.
+        /// </summary>
+        /// <param name="node">The complication (root) node</param>
+        public override void VisitCompilationUnit(CompilationUnitSyntax node)
+        {
+            ArgumentValidation.NotNull(node, "node");
+
+            foreach (var child in node.Members)
+            {
+                this.Visit(child);
+            }
+        }
+
+        /// <summary>
+        /// Visits a namespace declaration.
+        /// </summary>
+        /// <param name="node">The namespace declaration node</param>
+        public override void VisitNamespaceDeclaration(NamespaceDeclarationSyntax node)
+        {
+            ArgumentValidation.NotNull(node, "node");
+
+            foreach (var child in node.Members)
+            {
+                this.Visit(child);
+            }
+        }
+
+        private void VisitTypeDeclaration(TypeDeclarationSyntax node)
+        {
+            ArgumentValidation.NotNull(node, "node");
+
+            this.FireOnNodeFound(node, SyntaxNodeKind.Type, node.Identifier);
+            foreach (var child in node.Members)
+            {
+                this.Visit(child);
+            }
+        }
+
+        public override void VisitClassDeclaration(ClassDeclarationSyntax node)
+            => VisitTypeDeclaration(node);
+
+        public override void VisitRecordDeclaration(RecordDeclarationSyntax node)
+            => VisitTypeDeclaration(node);
+
+        public override void VisitStructDeclaration(StructDeclarationSyntax node)
+            => VisitTypeDeclaration(node);
+
+        public override void VisitInterfaceDeclaration(InterfaceDeclarationSyntax node)
+            => VisitTypeDeclaration(node);
+
+        public override void VisitEnumDeclaration(EnumDeclarationSyntax node)
+        {
+            ArgumentValidation.NotNull(node, "node");
+            this.FireOnNodeFound(node, SyntaxNodeKind.Type, node.Identifier);
+        }
+
+        /// <summary>
+        /// Visits a method declaration.
+        /// </summary>
+        /// <param name="node">The method declaration</param>
+        public override void VisitMethodDeclaration(MethodDeclarationSyntax node)
+        {
+            ArgumentValidation.NotNull(node, "node");
+            this.FireOnNodeFound(node, SyntaxNodeKind.Method, node.Identifier);
+        }
+
+        /// <summary>
+        /// The constructor declaration.
+        /// </summary>
+        /// <param name="node">The constructor declaration</param>
+        public override void VisitConstructorDeclaration(ConstructorDeclarationSyntax node)
+        {
+            ArgumentValidation.NotNull(node, "node");
+            this.FireOnNodeFound(node, SyntaxNodeKind.Method, node.Identifier);
+        }
+
+        /// <summary>
+        /// Visits a destructor declaration.
+        /// </summary>
+        /// <param name="node">The descructor declaration</param>
+        public override void VisitDestructorDeclaration(DestructorDeclarationSyntax node)
+        {
+            ArgumentValidation.NotNull(node, "node");
+            this.FireOnNodeFound(node, SyntaxNodeKind.Method, node.TildeToken);
+        }
+
+        /// <summary>
+        /// Visits an operator declaration.
+        /// </summary>
+        /// <param name="node">The operator declaration</param>
+        public override void VisitOperatorDeclaration(OperatorDeclarationSyntax node)
+        {
+            ArgumentValidation.NotNull(node, "node");
+            this.FireOnNodeFound(node, SyntaxNodeKind.Method, node.OperatorKeyword);
+        }
+
+        /// <summary>
+        /// Visits a property declaration.
+        /// </summary>
+        /// <param name="node">The property declaration</param>
+        public override void VisitPropertyDeclaration(PropertyDeclarationSyntax node)
+        {
+            ArgumentValidation.NotNull(node, "node");
+            this.FireOnNodeFound(node, SyntaxNodeKind.Property, node.Identifier);
+        }
+
+        /// <summary>
+        /// Visits a indexer
+        /// </summary>
+        public override void VisitIndexerDeclaration(IndexerDeclarationSyntax node)
+        {
+            ArgumentValidation.NotNull(node, "node");
+            this.FireOnNodeFound(node, SyntaxNodeKind.Property, node.ThisKeyword);
+        }
+
+        /// <summary>
+        /// Override the default visit method to throw if we're cancelled.
+        /// </summary>
+        /// <param name="node">The syntax node</param>
+        public override void Visit(SyntaxNode node)
+        {
+            this.cancellationToken.ThrowIfCancellationRequested();
+            base.Visit(node);
+        }
+
+        /// <summary>
+        /// Validates node state prior to firing OnNodeFound
+        /// </summary>
+        /// <param name="node">Syntax Node</param>
+        /// <param name="kind">Node Kind</param>
+        /// <param name="identifier">Identifier token of the node</param>
+        private void FireOnNodeFound(SyntaxNode node, SyntaxNodeKind kind, SyntaxToken identifier)
+        {
+            if (identifier.Span.Length > 0)
+            {
+                this.onNodeFound(new CSharpNodeInfo(node, kind, identifier));
+            }
+        }
+
+        /// <summary>
+        /// This class is returned to callers when we find a new node.
+        /// </summary>
+        private class CSharpNodeInfo : INodeInfo
+        {
+            private readonly SyntaxNode node;
+            private readonly SyntaxNodeKind kind;
+            private int startPosition = 0;
+
+            /// <summary>
+            /// Creates the CSharp node found info object.
+            /// </summary>
+            /// <param name="node">The node that was found</param>
+            /// <param name="kind">The kind of node</param>
+            /// <param name="identifier">The token in the node on which the tag should be created</param>
+            public CSharpNodeInfo(SyntaxNode node, SyntaxNodeKind kind, SyntaxToken identifier)
+            {
+                this.node = node;
+                this.kind = kind;
+
+                if (identifier == default(SyntaxToken))
+                {
+                    this.startPosition = node.Span.Start;
+                }
+                else
+                {
+                    var lines = identifier.SyntaxTree.GetText().Lines;
+                    var line = lines.GetLineFromPosition(identifier.Span.Start);
+
+                    // check if start of line is within current node
+                    this.startPosition = node.FullSpan.Contains(line.Start) ? node.FindToken(line.Start).Span.Start : node.Span.Start;
+                }
+            }
+
+            /// <summary>
+            /// Returns the starting position of the node.
+            /// </summary>
+            public int Start
+            {
+                get
+                {
+                    return this.startPosition;
+                }
+            }
+
+            /// <summary>
+            /// Returns a new syntax node info object for this found node.
+            /// </summary>
+            /// <param name="snapshot">The text snapshot that the locator uses to translate positions between versions</param>
+            public SyntaxNodeInfo CreateSyntaxNodeInfo(ITextSnapshot snapshot)
+            {
+                return new SyntaxNodeInfo(this.node, this.kind, new SyntaxNodeTracker(this.node, snapshot, this.startPosition));
+            }
+        }
+    }
+}
