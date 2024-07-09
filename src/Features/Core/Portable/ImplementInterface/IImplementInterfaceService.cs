d1e28b9848fcae63de24a463e44d7065cc8bfae3
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-07-09 14:43:03 -0700

    Share more

--- src/Features/Core/Portable/ImplementInterface/IImplementInterfaceService.cs
+++ src/Features/Core/Portable/ImplementInterface/IImplementInterfaceService.cs
@@ -11,8 +11,30 @@ using Microsoft.CodeAnalysis.ImplementType;
 
 namespace Microsoft.CodeAnalysis.ImplementInterface;
 
+internal interface IImplementInterfaceInfo
+{
+
+}
+
+internal readonly record struct ImplementInterfaceOptions(
+    bool Explicitly,
+    bool Abstractly,
+    bool OnlyRemaining,
+    ISymbol? ThroughMember)
+{
+}
+
 internal interface IImplementInterfaceService : ILanguageService
 {
     Task<Document> ImplementInterfaceAsync(Document document, ImplementTypeGenerationOptions options, SyntaxNode node, CancellationToken cancellationToken);
-    ImmutableArray<CodeAction> GetCodeActions(Document document, ImplementTypeGenerationOptions options, SemanticModel model, SyntaxNode node, CancellationToken cancellationToken);
+
+    Task<IImplementInterfaceInfo> GetInfoAsync(Document document, SyntaxNode node, CancellationToken cancellationToken);
+    Task<Document> ImplementInterfaceAsync(
+        Document document,
+        IImplementInterfaceInfo info,
+        ImplementTypeGenerationOptions typeGenerationOptions,
+        ImplementInterfaceOptions implementInterfaceOptions,
+        CancellationToken cancellationToken);
+
+    // ImmutableArray<CodeAction> GetCodeActions(Document document, ImplementTypeGenerationOptions options, SemanticModel model, SyntaxNode node, CancellationToken cancellationToken);
 }
