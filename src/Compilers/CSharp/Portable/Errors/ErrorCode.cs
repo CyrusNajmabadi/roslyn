4f1e1b6adca251341e0c1dbbb200946f104d401b
Cyrus Najmabadi <Cyrus Najmabadi>
2026-04-16 11:24:44 +0200

    Refactor labeled break/continue binding to walk binder chain
    Instead of doing a separate label lookup + syntax inspection, BreakLabel
    and ContinueLabel are now methods that take an optional label name and
    walk the binder chain naturally. LoopBinder and SwitchBinder detect
    their immediately-attached label at construction time.
    
    Consolidates three error codes into ERR_NoBreakOrContId.

--- src/Compilers/CSharp/Portable/Errors/ErrorCode.cs
+++ src/Compilers/CSharp/Portable/Errors/ErrorCode.cs
@@ -2489,6 +2489,7 @@ namespace Microsoft.CodeAnalysis.CSharp
         ERR_LabeledBreakContinueNotOnLoopOrSwitch = 9379,
         ERR_LabeledContinueNotOnLoop = 9380,
         ERR_LabeledBreakContinueNotContaining = 9381,
+        ERR_NoBreakOrContId = 9382,
 
         // Note: you will need to do the following after adding errors:
         //  1) Update ErrorFacts.IsBuildOnlyDiagnostic (src/Compilers/CSharp/Portable/Errors/ErrorFacts.cs)
